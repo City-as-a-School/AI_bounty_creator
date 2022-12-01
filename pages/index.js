3dea33491aef00067f783ed60732039293099f8d
CASAdmin <117321681+CASAdmin@users.noreply.github.com>
2022-12-01 22:51:03 +0000

    Working Stable with API key

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,81 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+//import CityasaschoolLogo from '../assets/buildspace-logo.png';
 
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>ARK | City as a School</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Access all human Knowledge in the blink of an AI</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Ask a question, get a course in seconds</h2>
           </div>
         </div>
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+      <div className="prompt-container">
+        <textarea
+          placeholder="start typing here"
+          className="prompt-box"
+          value={userInput}
+          onChange={onUserChangedText}
+        />
+
+        <div className="prompt-buttons">
+          <a className={isGenerating ? 'generate-button loading' : 'generate-button'} 
+            onClick={callGenerateEndpoint}
+          >
+            <div className="generate">
+            {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
+             
+            </div>
+          </a>
+        </div>
+
+        <div className="output">
+          <div className="output-header-container">
+            <div className="output-header">
+              <h3>Output</h3>
+            </div>
           </div>
-        </a>
+          <div className="output-content">
+            <p>{apiOutput}</p>
+          </div>
+        </div>
+
       </div>
     </div>
   );
